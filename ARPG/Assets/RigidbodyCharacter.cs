92a34260b639424f68b7554d7359aff05aaee799
kimdol <86827391+kimdol@users.noreply.github.com>
2022-03-20 02:39:21 +0900

    RigidBody를 이용한 캐릭터 완성

--- ARPG/Assets/RigidbodyCharacter.cs
+++ ARPG/Assets/RigidbodyCharacter.cs
@@ -21,7 +21,7 @@ public class RigidbodyCharacter : MonoBehaviour
     [SerializeField]
     private LayerMask groundLayerMask;
 
-    private bool isGrounded = true;
+    private bool isGrounded = false;
 
     [SerializeField]
     private float dashDistance = 5f;
@@ -31,12 +31,62 @@ public class RigidbodyCharacter : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        
+        rigidbody = GetComponent<Rigidbody>(); 
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        CheckGroundStatus();
+
+        // 사용자 입력값을 받아오기(이동)
+        inputDirection = Vector3.zero;
+        inputDirection = Vector3.zero;
+        inputDirection.x = Input.GetAxis("Horizontal");
+        inputDirection.z = Input.GetAxis("Vertical");
+        if (inputDirection != Vector3.zero)
+        {
+            transform.forward = inputDirection;
+        }
+
+        // 사용자 입력값을 받아오기(쩜프)
+        if (Input.GetButtonDown("Jump") && isGrounded)
+        {
+            rigidbody.AddForce(Vector3.up * Mathf.Sqrt(jumpHeight * -2f * Physics.gravity.y), ForceMode.VelocityChange);
+        }
+
+        // 사용자 입력값을 받아오기(대쉬)
+        if (Input.GetButtonDown("Dash"))
+        {
+            Vector3 dashVelocity = Vector3.Scale(transform.forward, dashDistance * new Vector3((Mathf.Log(1f / (Time.deltaTime * rigidbody.drag + 1)) / -Time.deltaTime), 0, (Mathf.Log(1f / (Time.deltaTime * rigidbody.drag + 1)) / -Time.deltaTime)));
+            rigidbody.AddForce(dashVelocity, ForceMode.VelocityChange);
+        }
     }
+    // Fixed Timestep 기반으로 호출되는 함수
+    private void FixedUpdate()
+    {
+        rigidbody.MovePosition(rigidbody.position + inputDirection * speed * Time.fixedDeltaTime);
+    }
+    #region Helper Methods
+    void CheckGroundStatus()
+    {
+        RaycastHit hitInfo;
+#if UNITY_EDITOR
+        // ground check Ray
+        Debug.DrawLine(transform.position + (Vector3.up * 0.1f), 
+            transform.position + (Vector3.up * 0.1f) + (Vector3.down * groundCheckDistance));
+#endif
+        // ground check
+        if (Physics.Raycast(transform.position + (Vector3.up * 0.1f), 
+            Vector3.down, out hitInfo, groundCheckDistance, groundLayerMask))
+        {
+            isGrounded = true;
+        }
+        else
+        {
+            isGrounded = false;
+        }
+    }
+    #endregion Helper Methods
+
 }
