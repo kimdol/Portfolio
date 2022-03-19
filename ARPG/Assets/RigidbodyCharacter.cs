bb664af53f069a7c5d3396abb7aa1c9dfebfdab4
kimdol <86827391+kimdol@users.noreply.github.com>
2022-03-20 00:24:37 +0900

    region 추가

--- ARPG/Assets/RigidbodyCharacter.cs
+++ ARPG/Assets/RigidbodyCharacter.cs
@@ -4,6 +4,30 @@ using UnityEngine;
 
 public class RigidbodyCharacter : MonoBehaviour
 {
+    #region Variables
+
+    [SerializeField]
+    private float speed = 5f;
+
+    private Rigidbody rigidbody;
+    private Vector3 inputDirection = Vector3.zero;
+
+    [SerializeField]
+    private float jumpHeight = 2f;
+
+    [SerializeField]
+    private float groundCheckDistance = 0.2f;
+
+    [SerializeField]
+    private LayerMask groundLayerMask;
+
+    private bool isGrounded = true;
+
+    [SerializeField]
+    private float dashDistance = 5f;
+
+    #endregion
+
     // Start is called before the first frame update
     void Start()
     {
