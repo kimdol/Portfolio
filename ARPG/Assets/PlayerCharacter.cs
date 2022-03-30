bf784d4e18550bf6617e7cd4c5733b5d4b04d935
kimdol <86827391+kimdol@users.noreply.github.com>
2022-03-30 22:50:48 +0900

    Lighting

--- ARPG/Assets/PlayerCharacter.cs
+++ ARPG/Assets/PlayerCharacter.cs
@@ -51,8 +51,6 @@ public class PlayerCharacter : MonoBehaviour
             RaycastHit hit;
             if (Physics.Raycast(ray, out hit, 100, groundLayerMask))
             {
-                // Debug.Log("We hit " + hit.collider.name + " " + hit.point);
-
                 // Hit한 곳에 이동
                 agent.SetDestination(hit.point);
             }
