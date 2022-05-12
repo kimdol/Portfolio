1c9e4bc7857c32a223b14b710854cb3dff95f919
kimdol <86827391+kimdol@users.noreply.github.com>
2022-05-12 21:15:14 +0900

    원거리 공격 완성

---
+++ ARPG/Assets/Projectile_Follow.cs
@@ -0,0 +1,33 @@
+using ARPG.Core;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Projectile_Follow : Projectile
+{
+    public float destroyDelay = 5.0f;
+
+    protected override void Start()
+    {
+        base.Start();
+
+        StartCoroutine(DestroyParticle(destroyDelay));
+    }
+
+    protected override void FixedUpdate()
+    {
+        if (target)
+        {
+            Vector3 dest = target.transform.position;
+            dest.y += 1.5f;
+            transform.LookAt(dest);
+        }
+
+        base.FixedUpdate();
+    }
+
+    protected override void OnCollisionEnter(Collision collision)
+    {
+        base.OnCollisionEnter(collision);
+    }
+}
