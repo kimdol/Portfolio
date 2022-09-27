3f5e5632f327c1fc6a936cf0f583243acf4b41e8
kimdol <86827391+kimdol@users.noreply.github.com>
2022-09-27 14:55:41 +0900

    LoginPanelControl_완성

---
+++ ARPG/Assets/FirebaseScripts/LoginSignupPanelControl.cs
@@ -0,0 +1,67 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEditor.VersionControl;
+
+public enum LSPanelType : int
+{
+    Popup_Signup = 0,
+    Popup_Login = 1,
+    MessageError = 2,
+    Default,
+}
+
+public class LoginSignupPanelControl : MonoBehaviour
+{
+    [Header("Default staticPanels: Popup_Signup = 0, Popup_Login = 1, MessageError = 2")]
+    public GameObject[] staticPanels = null;
+
+    private void Start()
+    {
+        var userId = FirebaseAuthManager.Instance.UserId;
+
+        if (userId == string.Empty)
+        {
+            staticPanels[(int)LSPanelType.Popup_Signup].SetActive(false);
+            staticPanels[(int)LSPanelType.Popup_Login].SetActive(true);
+            staticPanels[(int)LSPanelType.MessageError].SetActive(true);
+        }
+        else
+        {
+            staticPanels[(int)LSPanelType.Popup_Signup].SetActive(false);
+            staticPanels[(int)LSPanelType.Popup_Login].SetActive(false);
+            staticPanels[(int)LSPanelType.MessageError].SetActive(false);
+        }
+
+        FirebaseAuthManager.Instance.OnChangedLoginState += OnChangedLoginState;
+    }
+
+    public void OnClickLogin()
+    {
+        staticPanels[(int)LSPanelType.Popup_Signup].SetActive(false);
+        staticPanels[(int)LSPanelType.Popup_Login].SetActive(true);
+    }
+
+    public void OnClickSignup()
+    {
+        staticPanels[(int)LSPanelType.Popup_Signup].SetActive(true);
+        staticPanels[(int)LSPanelType.Popup_Login].SetActive(false);
+    }
+
+    private void OnChangedLoginState(bool loggined)
+    {
+        if (loggined)
+        {
+            staticPanels[(int)LSPanelType.Popup_Signup].SetActive(false);
+            staticPanels[(int)LSPanelType.Popup_Login].SetActive(false);
+            staticPanels[(int)LSPanelType.MessageError].SetActive(false);
+        }
+        else
+        {
+            staticPanels[(int)LSPanelType.Popup_Signup].SetActive(false);
+            staticPanels[(int)LSPanelType.Popup_Login].SetActive(true);
+            staticPanels[(int)LSPanelType.MessageError].SetActive(true);
+        }
+    }
+}
