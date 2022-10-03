31f68c1dfdbe7cdd9d5eca3ac79afa40371d977c
kimdol <86827391+kimdol@users.noreply.github.com>
2022-10-03 23:44:04 +0900

    DoorSystem_Added

---
+++ ARPG/Assets/ARPG/DoorSystem/DoorTriggerArea.cs
@@ -0,0 +1,28 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DoorTriggerArea : MonoBehaviour
+{
+    public DoorEventObject doorEventObject;
+    public DoorController doorController;
+
+    public bool autoClose = true;
+
+    private void OnTriggerEnter(Collider other)
+    {
+        doorEventObject.OpenDoor(doorController.id);
+    }
+
+    private void OnTriggerStay(Collider other)
+    {
+    }
+
+    private void OnTriggerExit(Collider other)
+    {
+        if (autoClose)
+        {
+            doorEventObject.CloseDoor(doorController.id);
+        }
+    }
+}
