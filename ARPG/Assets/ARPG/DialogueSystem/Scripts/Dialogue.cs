94cb183f4c860ccef0026f6ce053f4723c2e7dde
kimdol <86827391+kimdol@users.noreply.github.com>
2022-10-03 23:42:14 +0900

    DialogueSystem_Add

---
+++ ARPG/Assets/ARPG/DialogueSystem/Scripts/Dialogue.cs
@@ -0,0 +1,17 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace ARPG.DialogueSystem
+{
+    [Serializable]
+    public class Dialogue
+    {
+        public string name;
+
+        [TextArea(3, 10)]
+        public string[] sentences;
+    }
+}
+
