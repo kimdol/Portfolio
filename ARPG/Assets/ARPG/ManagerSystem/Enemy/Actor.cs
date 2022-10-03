b1afaf15f3436f55723795d1ce9721243698aea2
kimdol <86827391+kimdol@users.noreply.github.com>
2022-10-04 03:00:19 +0900

    EnemySquadronManager_추가

---
+++ ARPG/Assets/ARPG/ManagerSystem/Enemy/Actor.cs
@@ -0,0 +1,50 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Networking;
+
+namespace ARPG.Characters
+{
+    public class Actor : MonoBehaviour
+    {
+        [SerializeField]
+        protected bool isDead = false;
+
+        public bool IsDead
+        {
+            get
+            {
+                return isDead;
+            }
+        }
+
+
+        // Start is called before the first frame update
+        protected virtual void Start()
+        {
+            Initialize();
+        }
+
+        protected virtual void Initialize()
+        {
+
+        }
+
+        // Update is called once per frame
+        protected virtual void Update()
+        {
+            UpdateActor();
+        }
+
+        protected virtual void UpdateActor()
+        {
+
+        }
+
+        public virtual void OnDead()
+        {
+            Debug.Log(name + " OnDead");
+            isDead = true;
+        }
+    }
+}
